67e49bfd00025e93363d7c271bee5ad2295fee9b
jocelinevanessa <154084363+JocelineVanessa@users.noreply.github.com>
2024-12-16 20:59:56 +0100

    Agregar archivos de proyecto.

---
+++ ab-gestion hospitalaria.cpp
@@ -0,0 +1,12 @@
+﻿// ab-gestion hospitalaria.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "ab-gestion hospitalaria.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
