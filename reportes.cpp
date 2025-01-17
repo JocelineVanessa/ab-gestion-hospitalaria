f57fd7ce08882b36ac06c6984ca69cd52f6e606f
jocelinevanessa <154084363+JocelineVanessa@users.noreply.github.com>
2025-01-17 17:41:02 +0100

    funcion pacientes atendido y medicos disponibles

--- reportes.cpp
+++ reportes.cpp
@@ -6,11 +6,50 @@
 using namespace std;
 
 void Reportes::ReportePacientesAtendidos() {
-        cout << "Reporte de pacientes atendidos no implementado.\n";
+    ifstream file("pacientes.csv");
+    if (!file.is_open()) {
+        cerr << "No se pudo abrir el archivo de pacientes.\n";
+        return;
     }
+
+    cout << "Reporte de pacientes atendidos:\n";
+    cout << "-----------------------------------------\n";
+
+    string linea;
+    int contador = 0;
+    while (getline(file, linea)) {
+        if (!linea.empty()) {
+            ++contador;
+        }
+    }
+    file.close();
+
+    cout << "Pacientes atendidos: " << contador << "\n";
+    cout << "-----------------------------------------\n";
+}
+
 void Reportes::ReporteMedicosDisponibles() {
-        cout << "Reporte de medicos disponibles no implementado.\n";
+    ifstream file("medicos.csv");
+    if (!file.is_open()) {
+        cerr << "No se pudo abrir el archivo de medicos.\n";
+        return;
     }
+
+    cout << "Reporte de medicos disponibles:\n";
+    cout << "-----------------------------------------\n";
+
+    string linea;
+    int contador = 0;
+    while (getline(file, linea)) {
+        if (!linea.empty()) {
+            ++contador;
+        }
+    }
+    file.close();
+
+    cout << "Medicos disponibles: " << contador << "\n";
+    cout << "-----------------------------------------\n";
+}
 
 void Reportes::ReporteCitasPendientesPorDia() {
         cout << "Reporte de citas pendientes por dia no implementado.\n";
